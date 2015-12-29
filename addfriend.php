3e4f5e2a10d1b28f9e337175600e2846c505740a
Nathaniel Woodthorpe <NJWoodthorpe@gmail.com>
2015-12-29 09:48:28 -0500

    Fixed Duplicate Friend Issue

--- addfriend.php
+++ addfriend.php
@@ -8,12 +8,22 @@
     $uid2 = $_GET["uid2"];
     $uid1 = mysqli_real_escape_string($conn, $uid1);
     $uid2 = mysqli_real_escape_string($conn, $uid2);
-    $query = $conn->query("INSERT INTO friends 
-                (friend1, friend2)
-                VALUES('" . $uid1 . "', '" . $uid2. "')");
-    if($query){
-        echo "success";   
+
+    $query = $conn->query("SELECT * FROM friends 
+                WHERE ((friend1=" . $uid1 . " OR friend1=" . $uid2 . ")
+                AND
+                        (friend2=" . $uid1 . " OR friend2=" . $uid2 . "))");
+
+    if($query && $query->num_rows > 0){
+        echo "You can't add a friend twice!";
     }else{
-        echo "Error adding friend!";   
+        $query = $conn->query("INSERT INTO friends 
+                    (friend1, friend2)
+                    VALUES('" . $uid1 . "', '" . $uid2. "')");
+        if($query){
+            echo "success";   
+        }else{
+            echo "Error adding friend!";   
+        }
     }
 ?>
