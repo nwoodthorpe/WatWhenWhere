3f59f179d78cae9cfd1d82e4835ad141b5bad9f0
Nathaniel Woodthorpe <NJWoodthorpe@gmail.com>
2015-12-30 20:30:27 -0500

    Webcrawlers can't spam me now

--- contact.php
+++ contact.php
@@ -1,10 +1,18 @@
 <?php
+    if(!isset($_GET["message"]) || !isset($_GET["name"]) || !isset($_GET["email"])){
+        echo "Fill in all the boxes please!";
+        die();
+    }
     $email = $_GET["email"];
     $message = $_GET["message"];
     $name = $_GET["name"];
+    if($email == ""|| $message == "" || $name == ""){
+        echo "Fill in all the boxes please!";
+        die();
+    }
 
     $message = wordwrap($message, 70);
-
+    
     mail("contact@watwhenwhere.ca","A Message From " . $name,$message,"From: $email\n");
     echo "success";
 ?>
