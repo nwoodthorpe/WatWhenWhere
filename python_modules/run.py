9730949f2eb8e808db5202ec08e33fe08d1229ce
sarthfrey <sarth.frey@gmail.com>
2015-12-30 20:25:49 -0500

    Implemented collide and added more tests

--- python_modules/run.py
+++ python_modules/run.py
@@ -1,6 +1,7 @@
 import argparse
+import collision
 
 parser = argparse.ArgumentParser()
 parser.add_argument("tokens", nargs="+", help="enter non apostrophe contained tokens deliminated by spaces")
 args = parser.parse_args()
-print args.tokens
+print collision.collide(args.tokens)
